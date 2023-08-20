245281bbf3518b3644e69b48db816923e07b305a
MIkhail Nimchenko <nimchenko2013@mail.ru>
2023-08-20 13:54:23 +0400

    UI improvements

--- src/popup.ts
+++ src/popup.ts
@@ -8,11 +8,6 @@ const SwitchTypes = {
     HideUsersInDiscussions: 1,
 }
 
-const ClassTypes = {
-    activated: "on",
-    deactivated: "off"
-}
-
 async function SwitchValues(elemId: string, switchType: number) {
     const elem = document.getElementById(elemId);
     if (elem === null || switchType === -1)
@@ -21,32 +16,21 @@ async function SwitchValues(elemId: string, switchType: number) {
     async function invertAndSync(key: string): Promise<boolean> {
         let result = await chrome.storage.sync.get([key]);
         result[key] = !result[key];
-
-        const newValue: boolean = await chrome.storage.sync.set(result).then(() => result[key]);
-        return newValue;
+        return await chrome.storage.sync.set(result).then(() => result[key]);
     }
 
-    let result: boolean;
     switch (switchType) {
         case SwitchTypes.HideDiscussions: {
-            result = await invertAndSync(StorageKeys.HideDiscussions);
+            await invertAndSync(StorageKeys.HideDiscussions);
             break;
         }
         case SwitchTypes.HideUsersInDiscussions: {
-            result = await invertAndSync(StorageKeys.HideUsersInDiscussions)
+            await invertAndSync(StorageKeys.HideUsersInDiscussions)
             break;
         }
         default: {
             throw new Error("Incorrect switchType!");
         }
-    }
-
-    if (result) {
-        elem.classList.remove(ClassTypes.deactivated);
-        elem.classList.add(ClassTypes.activated);
-    } else {
-        elem.classList.remove(ClassTypes.activated);
-        elem.classList.add(ClassTypes.deactivated);
     }
 }
 
@@ -54,15 +38,9 @@ async function GetSyncValueByKey(key = '') {
     return (await chrome.storage.sync.get([key]))[key];
 }
 
-const hrefIDsAndLinks: string[][] = [
-    ['razrab_vk', 'https://vk.com/grafonmeister'],
-    ['razrab_youtube', 'https://www.youtube.com/@newgrafon5185'],
-    ['razrab_github', 'https://github.com/NewGrafon'],
-    ['sclab_vk', 'https://vk.com/stalcraftlab'],
-    ['sclab_github', 'https://github.com/Stalcraft-Dev-Team'],
-]
-
 const init = async () => {
+    const elementId = 'toggle-posts';
+    const togglePosts = document.getElementById(elementId)! as HTMLInputElement;
 
     if (await GetSyncValueByKey(StorageKeys.HideDiscussions) === undefined) {
         const obj = {};
@@ -71,28 +49,20 @@ const init = async () => {
         await chrome.storage.sync.set(obj);
     }
 
-
-    const hdKey = 'hd';
-    let hd = document.getElementById(hdKey);
-    if (hd && !hd.classList.contains('hasEvent')) {
-        hd.classList.add('hasEvent');
-        hd.addEventListener('click', () => {
-            SwitchValues(hdKey, SwitchTypes.HideDiscussions);
-        })
-        hd.classList.add(await GetSyncValueByKey(StorageKeys.HideDiscussions) ? ClassTypes.activated : ClassTypes.deactivated);
+    if (await GetSyncValueByKey(StorageKeys.HideDiscussions) === true && togglePosts) {
+        togglePosts.checked = true;
     }
 
-
-    // links
-    hrefIDsAndLinks.forEach(arr => {
-        let elem = document.getElementById(arr[0]);
-        if (elem && !elem.classList.contains('hasEvent')) {
-            elem.classList.add('hasEvent');
-            elem.addEventListener('click', () => {
-                chrome.tabs.create({ url: arr[1] }, (tab) => {})
-            });
-        }
-    })
+    if (togglePosts) {
+        togglePosts.addEventListener('change', (event: Event) => {
+            const isChecked = (event.target as HTMLInputElement).checked;
+            if (isChecked) {
+                SwitchValues(elementId, SwitchTypes.HideDiscussions);
+            } else {
+                SwitchValues(elementId, SwitchTypes.HideDiscussions);
+            }
+        });
+    }
 }
 
 init();
