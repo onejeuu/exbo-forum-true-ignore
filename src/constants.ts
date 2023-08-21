d084cfb16fce06b0fbbc235249413a31e0379188
NewGrafon <ultrafig73@gmail.com>
2023-08-21 19:59:38 +0400

    create file

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+export const MessagesTypes = {
+    DeleteDiscussionsSubscribe: "deleteDiscussionsSubscribe",
+    DeleteMessagesInDiscussions: "deleteMessagesInDiscussions",
+    CollectIgnoredUsers: "collectIgnoredUsers",
+}
+
+export const StorageKeys = {
+    HideDiscussions: "hideDiscussions",
+    HideMessages: 'hideMessages',
+    HideUsersInDiscussions: "hideUsersInDiscussions"
+}
+
+export const SwitchTypes = {
+    HideDiscussions: 0,
+    HideUsersInDiscussions: 1,
+}
+
+export const ClassTypes = {
+    HasEvent: "hasEvent",
+    HideElement: "hideElement"
+}
