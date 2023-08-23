ec48474cadd7d9ab62762328f6733c645a4ff5c4
NewGrafon <ultrafig73@gmail.com>
2023-08-23 23:51:47 +0400

    some changes

--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,7 @@ export async function GetStorageValue(key: string) {
     return (await chrome.storage.local.get([key]))[key];
 }
 
-export async function SetStorageValue(key: string, value: boolean) {
+export async function SetStorageValue(key: string, value: any) {
     const obj: any = {};
     obj[key] = value;
     await chrome.storage.local.set(obj)
